0ad4968de4b0a2b0a8cc83ff7f2e25ab58d34bd6
EgorikBro <150683992+EgorikBro@users.noreply.github.com>
2025-10-28 21:42:26 +0500

    Create EmptyMaze.cs

---
+++ EmptyMaze.cs
@@ -0,0 +1,17 @@
+namespace Mazes
+{
+    public static class EmptyMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            Move(robot, Direction.Right, width - 3);
+            Move(robot, Direction.Down, height - 3);
+        }
+
+        private static void Move(Robot robot, Direction direction, int stepsCount)
+        {
+            for (var i = 0; i < stepsCount; i++)
+                robot.MoveTo(direction);
+        }
+    }
+}
