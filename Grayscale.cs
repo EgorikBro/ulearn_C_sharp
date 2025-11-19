989ce1adc908aeda9310bff67367e957cfe24630
EgorikBro <150683992+EgorikBro@users.noreply.github.com>
2025-11-19 22:01:16 +0500

    Create Grayscale.cs

---
+++ Grayscale.cs
@@ -0,0 +1,22 @@
+namespace Recognizer;
+
+public static class GrayscaleTask
+{
+    public static double[,] ToGrayscale(Pixel[,] original)
+    {
+        var width = original.GetLength(0);
+        var height = original.GetLength(1);
+        var grayscale = new double[width, height];
+
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                var pixel = original[x, y];
+                grayscale[x, y] = (0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B) / 255.0;
+            }
+        }
+
+        return grayscale;
+    }
+}
